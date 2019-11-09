57703da90d92154e4440c8f7c814ad0f60db773c
mkhodeir <mostafasany.khodeir@hotmail.com>
2019-11-09 02:09:09 +0100

    Enable XamalC

--- Module/shellXamarin.Module.ElLa3eba/Views/FaceVerficationPage.xaml.cs
+++ Module/shellXamarin.Module.ElLa3eba/Views/FaceVerficationPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace shellXamarin.Module.ElLa3eba.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FaceVerficationPage : ContentPage
     {
         public FaceVerficationPage()
